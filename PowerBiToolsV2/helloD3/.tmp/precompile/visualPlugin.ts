73ff2f8bfcf5d463e9531dbd74763c16f0e7ae38
Ted Pattison <ted@tedpattison.net>
2019-01-17 12:47:43 -0500

    Updates

---
+++ PowerBiToolsV2/helloD3/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var helloD3BC56C2A4E3C6403FBF5158A155A73EF7_DEBUG = {
+        name: 'helloD3BC56C2A4E3C6403FBF5158A155A73EF7_DEBUG',
+        displayName: 'helloD3',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '2.3.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.helloD3BC56C2A4E3C6403FBF5158A155A73EF7.Visual(options),
+        custom: true
+    };
+}
---
+++ PowerBiToolsV3/helloD3v5/.tmp/drop/.tmp/build/src/visual.d.ts
@@ -0,0 +1,14 @@
+import "./../style/visual.less";
+import powerbi from "powerbi-visuals-api";
+import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
+import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
+import IVisual = powerbi.extensibility.visual.IVisual;
+export declare class Visual implements IVisual {
+    private svgRoot;
+    private ellipse;
+    private text;
+    private padding;
+    constructor(options: VisualConstructorOptions);
+    update(options: VisualUpdateOptions): void;
+    private RenderVisual;
+}
